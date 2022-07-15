76456812a20f7ff18ec9c6211def63f1ad3e2d4f
TATHAN <67022946+TathanDev@users.noreply.github.com>
2022-07-15 16:59:49 +0200

    Update map.js

--- commands/map.js
+++ commands/map.js
@@ -22,12 +22,13 @@ module.exports = {
             .setTitle("MAP")
             .setAuthor({ name: 'TATHAN'})
             .setTimestamp()
-            .setImage(map1);
+            .setImage("https://media.discordapp.net/attachments/714780801698496522/997505948237176932/aventure_map_1.png?width=960&height=540");
+            
 
 
             const message =  await interaction.reply({ embeds: [mapAventure1] })
            
-            } else if (bdd["adventure-level"][interaction.member.id] == "1") {
+            } else if (bdd["adventure-level"][interaction.member.id] == "2") {
 
 
                 const mapAventure2 = new MessageEmbed()
